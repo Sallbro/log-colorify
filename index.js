8a19257b6ecbb0647113fec35ca8bc51f65b405a
unknown <asusalman986@gmail.com>
2025-03-01 18:23:17 +0530

    remove unwanted code

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 
 var Log = (function () {
-    function Log() {
-
-    }
+    // success
     Log.success = function (message) {
         console.log("%c ".concat(message), 'color:green');
         console.log("%c salman", 'color:green');
 
     }
+    // danger
     Log.danger = function (message) {
         console.log("%c ".concat(message), 'color:red');
     }
+    // info
     Log.info = function (message) {
         console.log("%c ".concat(message), 'color:black;background:yellow');
     }
+    // custom
     Log.custom = function (message, obj) {
         console.log("%c ".concat(message), `color:${obj?.color};background:${obj?.bgcolor};font-family:${obj?.font};font-weight: ${obj?.weight}; font-size: ${obj?.size};text-shadow: ${obj?.shadow}`);
     }
